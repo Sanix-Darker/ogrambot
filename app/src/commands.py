3e42d7be6308a4ae3192f74d9385ec0c4d183b29
sanix-darker <s4nixd@gmail.com>
2020-07-25 21:17:37 +0100

    Update command format of /start

--- app/src/commands.py
+++ app/src/commands.py
@@ -12,26 +12,22 @@ from app.src.utils import get_lang_string_by_code, get_name, printLog, reject_bo
 # ---------------------------------------------
 # define a command callback function : /start
 def start_callback(bot, update):
-
     lang_code = update.message.from_user.language_code
-
     printLog("/start ", update.message.from_user)
     bot.send_message(chat_id=update.message.chat_id,
                      text="Hello " + get_name(update.message.from_user))
 
     # profil, options, help, add, revoke, news, complex search
     if reject_bots(bot, update):
-
         bot.send_message(chat_id=update.message.chat_id,
                          text=get_lang_string_by_code(lang_code, "WELCOME_MESSAGE") 
-                         + " " + str(update.message.chat_id) + " | https://ogramcloud.com/?chat_id=" 
+                         + " " + str(update.message.chat_id) + "\nClick : https://ogramcloud.com/?chat_id=" 
                          + str(update.message.chat_id))
 
 
 # The Help function
 def help_callback(bot, update):
     lang_code = update.message.from_user.language_code
-
     printLog("/help ", update.message.from_user)
     if reject_bots(bot, update):
         bot.send_message(chat_id=update.message.chat_id,
